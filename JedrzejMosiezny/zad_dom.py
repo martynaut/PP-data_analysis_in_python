2e829b298800127b48d240f0c9fb02808be02af8
Jedrzej Mosiezny <jedrzej.mosiezny@gmail.com>
2018-07-30 16:27:55 +0200

    zad dom

--- JedrzejMosiezny/zad_dom.py
+++ JedrzejMosiezny/zad_dom.py
@@ -22,14 +22,23 @@ if os.path.isfile('data/sum.csv'):
 
 else:
     file = open('data/sum.csv', "w+")
-
+suma0 = [0, 0, 0]
+suma1 = [0, 0, 0]
+suma2 = [0, 0, 0]
 for item in filelist:
     with open(item, 'r') as f:
         counter = 0
-        for line in f:     # line by line
-            print (line)
-            counter += 1
-            #print(counter)
+        for idx, line in enumerate(f):     # line by line
+            globals()['line%s' % idx] = [float(x) for x in line.strip().split(",")]
+            #print(line%s % idx)
+        suma0 = [x + y for x, y in zip(suma0, line0)]
+        suma1 = [x + y for x, y in zip(suma1, line1)]
+        suma2 = [x + y for x, y in zip(suma2, line2)]
+        counter += 1
+
+print(suma0)
+print(suma1)
+print(suma2)
 
 '''nowa_lista = []
 for item in filelist:
