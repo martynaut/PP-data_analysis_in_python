f36e7a3e214b9b5bf721234a261130b66a9a8814
grzymi <przemyslaw.grzymislawski@gmail.com>
2018-08-28 13:39:17 +0200

    liczenie sredniej i tworzenie wykresow z danych

---
+++ grzymi/multi/11_12_2013_test_P06/average_and_charts.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import os
+import time
+
+start = time.time()
+
+FOLDER_WITH_DATA = '.'
+files = os.walk(FOLDER_WITH_DATA)
+#print ([x for x in files])
+
+#print('pliki', files)
+
+lista_plikow = []
+
+for krotka in files:
+    for file in krotka[2]:
+        lista_plikow.append(krotka[0].replace('\\','/')+'/'+file)
+
+#print('lista plikow = ', lista_plikow)
+
+def poziom(level):
+    poziom = []
+    for files in lista_plikow:
+        if files.startswith('./p'+str(level)+'/p'):
+            poziom.append(files)
+    return poziom
+
+print(poziom(1))
+
+total = pd.DataFrame()
+
+for x in poziom(1):
+    df = pd.read_csv(x, sep='\t', skiprows=23, usecols=[1,2,3,4,5,6,7,8,9,16,17], names=['T1','T2','T3','T4','T5','T6','T7','T8','Tsuction','CO2','O2'], decimal=',')
+    #ave = df.tail(10)
+    file_name = x[5:]
+    #print (file_name, '\n', df.tail(10).mean(axis=0).transpose())
+    ave = df.tail(10).mean(axis=0)
+    total = total.append(ave, ignore_index=True)
+
+print(total)
+
+end = time.time()
+print('Czas działania programu = ', end-start, 's')
+
+#https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.append.html
